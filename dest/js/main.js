6755613083447a51588197d8d75eca830afc58b3
mohamedshukry <mohamedshkry188@gmail.com>
2018-01-04 20:14:11 +0200

    top-navbor

---
+++ dest/js/main.js
@@ -0,0 +1,80 @@
+onmouseover
+
+
+$(document).ready(function(){
+	$(".list-group-item1").hover(function(){
+		$(".drop-menu").fadeIn(600);
+	});
+});
+
+ 
+function desabal(){
+var arry = [0,1];
+	function all(){
+		var arryAll=[];
+	}
+ return 
+}
+
+var add = (function () {
+    var counter = 0;
+    return function () {return counter += 1;}
+})();
+
+function myFunction(){
+    document.getElementById("demo").innerHTML = add();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function(){
+// 	$(document).ready(function(){
+// 	    $("#demo").hover(function(){
+// 	        $(this).css("opacity", ".5");
+// 	        }, function(){
+// 	        $(this).css("opacity", "1");
+// 	    });
+// 	});
+// });
