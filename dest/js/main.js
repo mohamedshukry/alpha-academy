df64c923f2ce6f8cea7d1601ba1beeecfa4034e8
mohamedshukry <mohamedshkry188@gmail.com>
2018-01-27 10:12:30 +0200

    add news and teachercard and map and edit testmanels

--- dest/js/main.js
+++ dest/js/main.js
@@ -37,3 +37,83 @@ $(function () {
         $("html, body").animate({scrollTop: 0}, 2000);
         return false;
     });
+    
+//======= owl carousel =======//
+  	$(document).ready(function(){
+	 var owl = $('.owl-carousel');
+		owl.owlCarousel({
+		    loop:true,
+		    margin:30,
+		    dots:true,
+		    rtl:true,
+		    nav:true,
+		    items:4,
+		    responsive:{
+		       0:{
+		           items:1,
+		       },
+		       576:{
+		           items:2,
+		       },
+		       992:{
+		           items:4,
+		       }
+		   },
+		    autoplay:true,
+		    animateIn: 'fadeUp',
+            animateOut: 'fadeOut',
+		    autoplayTimeout:5000,
+		    responsiveClass:true,
+		    autoplayHoverPause:false,
+		});
+		$('.play').on('click',function(){
+		    owl.trigger('play.owl.autoplay',[1000])
+		})
+		$('.stop').on('click',function(){
+		    owl.trigger('stop.owl.autoplay')
+		})
+	});
+$(document).ready(function(){
+    $("#card-hover").hover(function(){
+    	$("#card-background").css("background-color","#d47d11")
+        $("#card-show").slideDown("");
+    },
+     function(){
+        $("#card-show").slideUp("slow");
+        $("#card-background").css("background-color","#00aaff" ,"slow")
+    });
+});
+//======= counter	=======// 
+  	$('.count .timer').countTo();
+
+  	//======= owl carousel =======//
+  	$(document).ready(function(){
+	 var owl = $('.owl-carousel');
+		owl.owlCarousel({
+		    loop:true,
+		    margin:10,
+		    dots:true,
+		    rtl:true,
+		    nav:false,
+		    items:1,
+		    autoplay:true,
+		    animateIn: 'fadeUp',
+            animateOut: 'fadeOut',
+		    autoplayTimeout:5000,
+		    responsiveClass:true,
+		    autoplayHoverPause:false,
+		});
+		$('.play').on('click',function(){
+		    owl.trigger('play.owl.autoplay',[1000])
+		})
+		$('.stop').on('click',function(){
+		    owl.trigger('stop.owl.autoplay')
+		})
+	});
+	$(document).ready(function(){
+		$("#card").on('click',function(){
+			$("#card").css("border","2px solid #cc9900");
+			$("#card i").css("color","#cc9900");
+			$("button").toggle();
+		});
+	});	
